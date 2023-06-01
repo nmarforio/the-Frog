fac85beea1aa39d029ce8f00a193ac5b1f155298
python-game <n.marforio@gmail.com>
2023-06-01 11:07:20 +0200

    add the score, finished the point and did the game over

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ pygame.display.set_icon(icon)
 
 # score
 score = 0
+textFont = pygame.font.Font("freesansbold.ttf", 25)
+
+
+def showScore():
+    scoreText = textFont.render("score:" + str(score), True, (255, 255, 255))
+    wn.blit(scoreText, (10, 10))
 
 
 # Player the Frog
@@ -46,7 +52,7 @@ num_flies = 6
 for i in range(num_flies):
     flies.append(pygame.image.load("fly.png"))
     fliesX.append(random.randint(32, 968))
-    fliesY.append(random.randint(0, 500))
+    fliesY.append(random.randint(32, 500))
 
 
 def flies_targets(x, y, i):
@@ -62,7 +68,7 @@ num_butterflies = 4
 for j in range(num_butterflies):
     butterfly.append(pygame.image.load("butterfly.png"))
     butterflyX.append(random.randint(32, 968))
-    butterflyY.append(random.randint(0, 500))
+    butterflyY.append(random.randint(32, 500))
 
 
 def butterflies_targets(x, y, j):
@@ -78,7 +84,7 @@ num_leaves = 8
 for h in range(num_leaves):
     leave.append(pygame.image.load("leaves.png"))
     leaveX.append(random.randint(32, 968))
-    leaveY.append(random.randint(0, 500))
+    leaveY.append(random.randint(32, 500))
 
 
 def leave_targets(x, y, h):
@@ -88,7 +94,7 @@ def leave_targets(x, y, h):
 # branch1: Y 0-150
 branch = pygame.image.load("branch.png")
 branch1X = random.randint(0, 1000)
-branch1Y = random.randint(0, 150)
+branch1Y = random.randint(10, 150)
 branch1X_change = 1
 branch1Y_change = 40
 
@@ -153,16 +159,43 @@ def collisionBranch3(branch3X, branch3Y, playerX, playerY):
 
 
 # collision with Flies-targets
-
-
 def collisionFlies(fliesX, fliesY, playerX, playerY):
     distance = math.sqrt(
         (math.pow(fliesX - playerX, 2)) + (math.pow(fliesY - playerY, 2))
     )
-    if distance < 27:
+    if distance < 35:
         return True
     else:
         return False
+
+
+# collision with Butterflies-Targerts
+def collisionButterflies(butterfliesX, butterfliesY, playerX, playerY):
+    distance = math.sqrt(
+        (math.pow(butterfliesX - playerX, 2)) + (math.pow(butterfliesY - playerY, 2))
+    )
+    if distance < 35:
+        return True
+    else:
+        return False
+
+
+# collision with Leaves-Targets
+def collisionLeaves(leavesX, leavesY, playerX, playerY):
+    distance = math.sqrt(
+        (math.pow(leavesX - playerX, 2)) + (math.pow(leavesY - playerY, 2))
+    )
+    if distance < 35:
+        return True
+    else:
+        return False
+
+
+# Game Over
+def game_over():
+    fontText = pygame.font.Font("freesansbold.ttf", 64)
+    gameOverText = fontText.render("GAME OVER", True, (255, 255, 255))
+    wn.blit(gameOverText, (280, 300))
 
 
 # Game running
@@ -177,12 +210,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    #
+
     # Player movements
     keys = pygame.key.get_pressed()
+
     if keys[pygame.K_DOWN]:
         playerY += vel
+
     if not (isJump):
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_SPACE]:
             isJump = True
     elif isJump:
         if jumpCount >= -10:
@@ -190,20 +227,30 @@ while running:
             if jumpCount < 0:
                 neg = -1
             if end_jump3:
-                playerY = branch3Y + 10
                 jumpCount = 10
+                playerY = branch3Y
+                isJump = True
             if end_jump2:
-                playerY = branch2Y + 10
                 jumpCount = 10
+                playerY = branch2Y
+                isJump = True
             if end_jump1:
-                playerY = branch1Y + 10
                 jumpCount = 10
+                playerY = branch1Y
+                isJump = True
+
             else:
                 playerY -= (jumpCount**2) * 0.5 * neg
                 jumpCount -= 1
         else:
             isJump = False
             jumpCount = 10
+
+    # GAME-OVER
+    if score < 0:
+        game_over()
+        playerX = 436
+        playerY = 600
 
     # Player boundries
     playerX += player_changeX
@@ -269,28 +316,43 @@ while running:
 
     # flies loop
     for i in range(num_flies):
+        fliesCollision = collisionFlies(fliesX[i], fliesY[i], playerX, playerY)
+        if fliesCollision:
+            score += 6
+            fliesX[i] = random.randint(0, 968)
+            fliesY[i] = random.randint(0, 500)
+            if fliesX[i] and fliesY[i] == butterflyX[j] and butterflyY[j]:
+                fliesX[i] = random.randint(0, 968)
+            print(score)
+
         flies_targets(fliesX[i], fliesY[i], i)
 
     # butteflies loop
     for j in range(num_butterflies):
+        butterfliesCollision = collisionButterflies(
+            butterflyX[j], butterflyY[j], playerX, playerY
+        )
+        if butterfliesCollision:
+            score += 9
+            butterflyX[j] = random.randint(0, 968)
+            butterflyY[j] = random.randint(0, 500)
+            print(score)
         butterflies_targets(butterflyX[j], butterflyY[j], j)
 
     # leaves loop
     for h in range(num_leaves):
+        leavesCollision = collisionLeaves(leaveX[h], leaveY[h], playerX, playerY)
+        if leavesCollision:
+            score -= 6
+            leaveX[h] = random.randint(0, 968)
+            leaveY[h] = random.randint(0, 500)
         leave_targets(leaveX[h], leaveY[h], h)
-
-    # collisionTarget
-    fliesCollision = collisionFlies(fliesX[i], fliesY[i], playerX, playerY)
-    if fliesCollision:
-        score += 3
-        fliesX[i] = random.randint(0, 968)
-        fliesY[i] = random.randint(0, 500)
-        print(score)
 
     # function called
     player_moves(playerX, playerY)
     branch1(branch1X, branch1Y)
     branch2(branch2X, branch2Y)
     branch3(branch3X, branch3Y)
+    showScore()
 
     pygame.display.update()
